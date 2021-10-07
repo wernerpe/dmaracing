a45192759731b78b35f660a73105cf9938a2120e
Peter <wernerpe@mit.edu>
2021-10-07 18:58:44 -0400

    drawing single actor working

--- dmaracing/env/viewer.py
+++ dmaracing/env/viewer.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+from numpy.core.numeric import isclose
 import torch
 import numpy as np
 
@@ -10,6 +11,16 @@ class Viewer:
         self.height = self.cfg['viewer']['height']
         self.scale_x = self.cfg['viewer']['scale']
         self.scale_y = self.height/self.width*self.scale_x
+        self.thickness = self.cfg['viewer']['linethickness']
+        
+        #bounding box of car in model frame
+        w = self.cfg['model']['w']
+        lf = self.cfg['model']['lf']  
+        lr = self.cfg['model']['lr']
+        self.car_box_m = np.array([[lf, -w/2],[lf, w/2],[-lr, w/2], [-lr, -w/2]])
+        self.car_heading_m = np.array([[0, 0],[lf, 0]])
+        
+
 
         self.canvas = 255*np.ones((self.height, self.width, 3), np.uint8)
         cv.imshow('dmaracing', self.canvas)
@@ -17,7 +28,22 @@ class Viewer:
     def render(self, state):
         #do drawing
         #listen for keypressed events
-        self.canvas[35:45,35:45, :] = self.canvas[35:45,35:45, :] + 10 
-        cv.imshow("dmaracing", self.canvas)
+        img = 255*np.ones((self.height, self.width, 3), np.uint8)
+        state = state.cpu().numpy()
+        transl = state[0,0,0:2]
+        theta = state[0,0,2]
+        R = np.array([[np.cos(theta), np.sin(theta)],[-np.sin(theta), np.cos(theta)]])
+        car_box_world = (R@self.car_box_m.T + np.tile(transl.reshape(2,1), (1,4))).T 
+        car_heading_world = (R@self.car_heading_m.T + np.tile(transl.reshape(2,1), (1,2))).T 
+        px_car_box_world = self.cords2px(car_box_world).reshape(-1,1,2)
+        px_car_heading_world = self.cords2px(car_heading_world).reshape(-1,1,2)
+        cv.polylines(img, [px_car_box_world], isClosed = True, color = (0,0,0), thickness = self.thickness)
+        cv.polylines(img, [px_car_heading_world], isClosed = False, color = (0,0,0), thickness = self.thickness)
+        cv.imshow("dmaracing", img)
         key = cv.waitKey(1)
         return key
+
+    def cords2px(self, pts):
+        pts[:, 0] = (self.width/self.scale_x*pts[:, 0] + self.width/2.0).astype(np.int)
+        pts[:, 1] = (self.height/self.scale_y*pts[:, 1] + self.height/2.0).astype(np.int)
+        return pts.astype(np.int32)
--- scripts/test_viewer.py
+++ scripts/test_viewer.py
@@ -13,10 +13,10 @@ cfg, cfg_train = getcfg(path_cfg)
 viewer = Viewer(cfg)
 state_dummy = torch.zeros((2,2,7), dtype= torch.float, requires_grad=False, device='cuda:0')
 state_dummy[:, 1, 0] = 0.5
-state_dummy[:, :, 2] = np.pi/2
+state_dummy[:, :, 2] = np.pi/6
 
 while True:
-    evnt = viewer.render(state_dummy)
+    evnt = viewer.render(state_dummy[:,:,0:3])
     if evnt == 118:
         print('toggle viewer')
     if evnt == 113 or evnt == 27:
