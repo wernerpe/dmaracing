32e3c2663deb0bbfef9c0ccbb8f095cca00df966
Peter <wernerpe@mit.edu>
2021-10-07 20:26:02 -0400

    simple rendering done

--- dmaracing/env/viewer.py
+++ dmaracing/env/viewer.py
@@ -5,6 +5,7 @@ import numpy as np
 
 class Viewer:
     def __init__(self, cfg):
+        self.device = 'cuda:0'
         self.cfg = cfg
         #load cfg
         self.width = self.cfg['viewer']['width']
@@ -12,38 +13,59 @@ class Viewer:
         self.scale_x = self.cfg['viewer']['scale']
         self.scale_y = self.height/self.width*self.scale_x
         self.thickness = self.cfg['viewer']['linethickness']
-        
+        self.num_agents = self.cfg['sim']['numAgents']
+
         #bounding box of car in model frame
         w = self.cfg['model']['w']
         lf = self.cfg['model']['lf']  
         lr = self.cfg['model']['lr']
-        self.car_box_m = np.array([[lf, -w/2],[lf, w/2],[-lr, w/2], [-lr, -w/2]])
-        self.car_heading_m = np.array([[0, 0],[lf, 0]])
-        
-
-
+        self.car_box_m = torch.tensor([[lf, -w/2],[lf, w/2],[-lr, w/2], [-lr, -w/2]], device = self.device)
+        self.car_box_m = self.car_box_m.unsqueeze(2).repeat(1, 1, self.num_agents)
+        self.car_box_m = torch.transpose(self.car_box_m, 0,1) 
+        self.car_heading_m = torch.tensor([[0, 0],[lf, 0]], device = self.device)
+        self.car_heading_m = self.car_heading_m.unsqueeze(2).repeat(1, 1, self.num_agents)
+        self.car_heading_m = torch.transpose(self.car_heading_m, 0,1)
+        self.R = torch.zeros((2, 2, self.num_agents), device=self.device, dtype = torch.float)
         self.canvas = 255*np.ones((self.height, self.width, 3), np.uint8)
+        self.colors = 255.0/self.num_agents*np.arange(self.num_agents) 
+        self.font = cv.FONT_HERSHEY_SIMPLEX
+
         cv.imshow('dmaracing', self.canvas)
 
     def render(self, state):
         #do drawing
         #listen for keypressed events
         img = 255*np.ones((self.height, self.width, 3), np.uint8)
-        state = state.cpu().numpy()
-        transl = state[0,0,0:2]
-        theta = state[0,0,2]
-        R = np.array([[np.cos(theta), np.sin(theta)],[-np.sin(theta), np.cos(theta)]])
-        car_box_world = (R@self.car_box_m.T + np.tile(transl.reshape(2,1), (1,4))).T 
-        car_heading_world = (R@self.car_heading_m.T + np.tile(transl.reshape(2,1), (1,2))).T 
-        px_car_box_world = self.cords2px(car_box_world).reshape(-1,1,2)
-        px_car_heading_world = self.cords2px(car_heading_world).reshape(-1,1,2)
-        cv.polylines(img, [px_car_box_world], isClosed = True, color = (0,0,0), thickness = self.thickness)
-        cv.polylines(img, [px_car_heading_world], isClosed = False, color = (0,0,0), thickness = self.thickness)
+        transl = state[0,:,0:2]
+        theta = state[0,:,2]
+        self.R[0,0,:] = torch.cos(theta)
+        self.R[0,1,:] = torch.sin(theta)
+        self.R[1,0,:] = -torch.sin(theta)
+        self.R[1,1,:] = torch.cos(theta)
+
+        car_box_rot = torch.einsum ('ijl, jkl -> ikl', self.R, self.car_box_m)
+        car_box_world = torch.transpose(car_box_rot + transl.T.unsqueeze(1).repeat(1,4,1), 0,1)
+        
+        car_heading_rot = torch.einsum ('ijl, jkl -> ikl', self.R, self.car_heading_m)
+        car_heading_world = torch.transpose(car_heading_rot + transl.T.unsqueeze(1).repeat(1,2,1), 0,1)
+
+        px_car_box_world = self.cords2px(car_box_world)
+        px_car_heading_world = self.cords2px(car_heading_world)
+
+        for idx in range(self.num_agents):
+            px_x_number = (self.width/self.scale_x*transl[idx, 0] + self.width/2.0).cpu().numpy().astype(np.int32).item()
+            px_y_number = (self.height/self.scale_y*transl[idx, 1] + self.height/2.0).cpu().numpy().astype(np.int32).item()
+            px_pts_car = px_car_box_world[..., idx].reshape(-1,1,2)
+            px_pts_heading = px_car_heading_world[..., idx].reshape(-1,1,2)
+            cv.polylines(img, [px_pts_car], isClosed = True, color = (int(self.colors[idx]),0,int(self.colors[idx])), thickness = self.thickness)
+            cv.polylines(img, [px_pts_heading], isClosed = True, color = (int(self.colors[idx]),0,int(self.colors[idx])), thickness = self.thickness)
+            cv.putText(img, str(idx), (px_x_number, px_y_number), self.font, 0.5, (int(self.colors[idx]),0,int(self.colors[idx])), 1, cv.LINE_AA)
         cv.imshow("dmaracing", img)
         key = cv.waitKey(1)
         return key
 
     def cords2px(self, pts):
-        pts[:, 0] = (self.width/self.scale_x*pts[:, 0] + self.width/2.0).astype(np.int)
-        pts[:, 1] = (self.height/self.scale_y*pts[:, 1] + self.height/2.0).astype(np.int)
+        pts = pts.cpu().numpy()
+        pts[:, 0, :] = self.width/self.scale_x*pts[:, 0, :] + self.width/2.0
+        pts[:, 1, :] = self.height/self.scale_y*pts[:, 1, :] + self.height/2.0
         return pts.astype(np.int32)
--- scripts/test_viewer.py
+++ scripts/test_viewer.py
@@ -11,9 +11,9 @@ path_cfg = os.getcwd() + '/cfg'
 cfg, cfg_train = getcfg(path_cfg)
 
 viewer = Viewer(cfg)
-state_dummy = torch.zeros((2,2,7), dtype= torch.float, requires_grad=False, device='cuda:0')
-state_dummy[:, 1, 0] = 0.5
-state_dummy[:, :, 2] = np.pi/6
+state_dummy = torch.zeros((2,cfg['sim']['numAgents'],7), dtype= torch.float, requires_grad=False, device='cuda:0')
+state_dummy[0, :, 0] = -1 + 2.0/cfg['sim']['numAgents']*torch.arange(cfg['sim']['numAgents'])
+state_dummy[0, :, 2] = 2.0*np.pi/cfg['sim']['numAgents']*torch.arange(cfg['sim']['numAgents'])
 
 while True:
     evnt = viewer.render(state_dummy[:,:,0:3])
