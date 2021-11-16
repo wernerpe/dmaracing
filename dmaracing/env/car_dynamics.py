64d32d4a76398cb981a3e18c00781fdcdfdf7811
Peter <wernerpe@mit.edu>
2021-11-15 21:44:26 -0500

    wip

--- dmaracing/env/car_dynamics.py
+++ dmaracing/env/car_dynamics.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from dmaracing.env.car_dynamics_utils import resolve_collsions
 
-@torch.jit.script
+#@torch.jit.script
 def step_cars(state : torch.Tensor, 
               actions : torch.Tensor,
               wheel_locations: torch.Tensor,
@@ -109,7 +109,7 @@ def step_cars(state : torch.Tensor,
     
 
     #check which tires are on track
-    wheels_on_track_segments_concat = 1.0 * (torch.einsum('etc, eawc  -> eawt', A_track, wheel_locations_world) - b_track +0.1>=0 )
+    wheels_on_track_segments_concat = 1.0 * (torch.einsum('etc, eawc  -> eawt', A_track, wheel_locations_world) - b_track.view(num_envs,1,1,-1) +0.1>=0 )
     wheels_on_track_segments[:] = torch.einsum('ejt, eawt -> eawj', S_track, wheels_on_track_segments_concat) >= 3.5
     wheel_on_track = torch.any(wheels_on_track_segments, dim = 3)
     f_tot = torch.sqrt(torch.square(f_force) +torch.square(p_force)) + 1e-9
