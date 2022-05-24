e50fda0813f032f258714f418b6bedad985eef26
Peter <wernerpe@mit.edu>
2022-05-24 15:43:55 -0400

    straight line racing interesting

--- scripts/playffa2.py
+++ scripts/playffa2.py
@@ -171,7 +171,7 @@ if __name__ == "__main__":
     cfg, cfg_train, logdir_root = getcfg(path_cfg, straightline=True)
     SOUND = False
 
-    chkpts = [-1,  1000]
+    chkpts = [375, 350]
     runs = [-1]*2
     cfg['sim']['numEnvs'] = 4
     cfg['sim']['numAgents'] = 2
